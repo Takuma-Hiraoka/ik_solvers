be915b5ccf4cb3a0046b33cdaf8b449c167e13f7
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2022-10-19 21:21:59 +0900

    for JAXON_RED chest

--- ik_constraint/src/Jacobian.cpp
+++ ik_constraint/src/Jacobian.cpp
@@ -263,9 +263,11 @@ namespace IK {
         for(size_t j=0;j<robot->numJoints();j++){
           if(jacobianColMap.find(robot->joint(j)) != jacobianColMap.end()){
             int col_idx = jacobianColMap[robot->joint(j)];
+	    double weight = 1.0;
+	    if((robot->joint(j)->name() == "CHEST_JOINT0") || (robot->joint(j)->name() == "CHEST_JOINT1") || (robot->joint(j)->name() == "CHEST_JOINT2")) weight = 10.0;
             for(size_t k=0;k<3;k++){
               for(size_t d=0;d<getJointDOF(robot->joint(j));d++){
-                jacobian.coeffRef(k,col_idx+d) = sign * CMJ(k,j+d);
+                jacobian.coeffRef(k,col_idx+d) = sign * CMJ(k,j+d) / weight;
               }
             }
           }
